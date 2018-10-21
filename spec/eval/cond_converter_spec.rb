c7876cdbd0039b4eeebabf5fe4876c8f1f919db5
tamura shingo <tamura.shingo@gmail.com>
2018-10-22 00:33:34 +0900

    add test pattern

--- spec/eval/cond_converter_spec.rb
+++ spec/eval/cond_converter_spec.rb
@@ -3,7 +3,7 @@ require 'rscheme/rscheme_exception'
 require 'rscheme/eval/cond_converter'
 
 RSpec.describe 'CondConveter' do
-  example '(cond ((> x 1) x) ((= x 0) 0) (else (- x 0)))' do
+  example '(cond ((> x 1) x) ((= x 0) 0) (else (- x 0))) => (IF (> x 1) X (IF (= X 0) 0 (- X 0)))' do
     source = '(cond ((> x 1) x) ((= x 0) 0) (else (- x 0)))'
     parser = Parser.new source
     exp = parser.parse
@@ -50,7 +50,7 @@ RSpec.describe 'CondConveter' do
     expect(result.cdr.cdr.cdr.car.cdr.cdr.cdr.car.cdr.cdr.car.value).to eq(0)
   end
 
-  example '(cond (true x) (false y) (else 0))' do
+  example '(cond (true x) (false y) (else 0)) => (IF TRUE X (IF FALSE Y 0))' do
     source = '(cond (true x) (false y) (else 0))'
     parser = Parser.new source
     exp = parser.parse
@@ -105,5 +105,44 @@ end
 
     expect(result.cdr.cdr.car.type).to eq(:value)
     expect(result.cdr.cdr.car.value).to eq(2)
+  end
+
+  example '(cond (1 (display "ok") (newline)) (else (display "ng") (newline))) => (if 1 (begin (display "ok") (newline)) (begin (display "ng") (newline)))' do
+    source = '(cond (1 (display "ok") (newline)) (else (display "ng") (newline)))'
+    parser = Parser.new source
+    exp = parser.parse
+
+    result = CondConverter.cond_to_if exp
+
+    # (if 1 (begin (display "ok") (newline)) (begin (display "ng") (newline)))
+    expect(result.car.type).to eq(:symbol)
+    expect(result.car.value).to eq("IF")
+
+    expect(result.cdr.car.type).to eq(:value)
+    expect(result.cdr.car.value).to eq(1)
+
+    expect(result.cdr.cdr.car.car.type).to eq(:symbol)
+    expect(result.cdr.cdr.car.car.value).to eq("BEGIN")
+
+    expect(result.cdr.cdr.car.cdr.car.car.type).to eq(:symbol)
+    expect(result.cdr.cdr.car.cdr.car.car.value).to eq("DISPLAY")
+
+    expect(result.cdr.cdr.car.cdr.car.cdr.car.type).to eq(:string)
+    expect(result.cdr.cdr.car.cdr.car.cdr.car.value).to eq("ok")
+
+    expect(result.cdr.cdr.car.cdr.cdr.car.car.type).to eq(:symbol)
+    expect(result.cdr.cdr.car.cdr.cdr.car.car.value).to eq("NEWLINE")
+
+    expect(result.cdr.cdr.cdr.car.car.type).to eq(:symbol)
+    expect(result.cdr.cdr.cdr.car.car.value).to eq("BEGIN")
+
+    expect(result.cdr.cdr.cdr.car.cdr.car.car.type).to eq(:symbol)
+    expect(result.cdr.cdr.cdr.car.cdr.car.car.value).to eq("DISPLAY")
+
+    expect(result.cdr.cdr.cdr.car.cdr.car.cdr.car.type).to eq(:string)
+    expect(result.cdr.cdr.cdr.car.cdr.car.cdr.car.value).to eq("ng")
+
+    expect(result.cdr.cdr.cdr.car.cdr.cdr.car.car.type).to eq(:symbol)
+    expect(result.cdr.cdr.cdr.car.cdr.cdr.car.car.value).to eq("NEWLINE")
   end
 end
